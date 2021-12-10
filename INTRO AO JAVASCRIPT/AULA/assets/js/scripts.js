b8462c7caecbb78b78f18fe53b20a1c7f930b7d1
Paulo César <paulo_cesarcanal@hotmail.com>
2021-12-09 23:23:22 -0300

    CONTADOR
    Muito bom, tive um problema, meu contador contava só para números menores de que zero, mas achei o problema, e resolvi, o código html, eu copiei da aula, mas o css eu criei!

--- INTRO AO JAVASCRIPT/AULA/assets/js/scripts.js
+++ INTRO AO JAVASCRIPT/AULA/assets/js/scripts.js
@@ -1,1 +1,14 @@
-console.log("hello word!");
+var currentNumberWraper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment()
+{
+    currentNumber = currentNumber + 1;
+    currentNumberWraper.innerHTML = currentNumber;
+}
+
+function decrement()
+{
+    currentNumber = currentNumber - 1;
+    currentNumberWraper.innerHTML = currentNumber;
+}
