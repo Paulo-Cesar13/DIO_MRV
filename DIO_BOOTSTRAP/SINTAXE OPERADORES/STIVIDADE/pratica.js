0dc58d94463e5af6a6b6d975a371d4d34c6e8994
Paulo César <paulo_cesarcanal@hotmail.com>
2021-12-19 08:04:21 -0300

    Função notas
    Bom parte da função esta funcionando, consigo receber os números  sendo digitados pelo o  usuário, mas não consigo exibir o resultado matematico

--- DIO_BOOTSTRAP/SINTAXE OPERADORES/STIVIDADE/pratica.js
+++ DIO_BOOTSTRAP/SINTAXE OPERADORES/STIVIDADE/pratica.js
@@ -1,6 +1,17 @@
-function comparador(num1, num2){
-    if(num1 == num2) {
-        
-    }
-} 
+function nota() {
+    alert('Digite a primeira nota:');
+    let nota1 = prompt();
+    alert('Digite a segunda nota:');
+    let nota2 = prompt();
+    alert('Digite a terceira nota:');
+    let nota3 = prompt();
+    alert('Digite a quarta nota:');
+    let nota4 = prompt();
+
+    let soma = (nota1 + nota2 + nota3 + nota4);
+    let media = media + (soma / 4);
+
+    alert(`Sua Média É ${media}`);
 }
+
+nota();
