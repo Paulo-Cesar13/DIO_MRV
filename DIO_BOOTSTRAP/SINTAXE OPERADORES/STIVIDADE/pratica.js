bdce34e7d122f477e33bdb9d3dcadbd76f347fa1
Paulo César <paulo_cesarcanal@hotmail.com>
2021-12-19 12:54:48 -0300

    Função
    Ainda estou tendo problemas

--- DIO_BOOTSTRAP/SINTAXE OPERADORES/STIVIDADE/pratica.js
+++ DIO_BOOTSTRAP/SINTAXE OPERADORES/STIVIDADE/pratica.js
@@ -8,12 +8,14 @@ function nota() {
     alert('Digite a quarta nota:');
     let nota4 = prompt();
 
-    alert('Suas notas: \n primeira nota: (${nota1});\n segunda nota: (nota2);\n terceira nota: (nota3);\n quarta nita: (nota4).')
+    alert(('Suas notas: \n primeira nota:` (${nota1});)`\n segunda nota: (nota2);`\n terceira nota: (nota3);\n quarta nita: (nota4).')
 
-    let soma = (nota1 + nota2 + nota3 + nota4);
-    let media = media + (soma / 4);
+    let soma = 0;
+    soma = soma = (nota1 + nota2 + nota3 + nota4);
+    let media = 0;
+    media = media + (soma / 4);
 
     alert(`Sua Média É ${media}`);
 }
 
-nota();
+nota(prompt());
